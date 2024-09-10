c71f8d94ecffbbd41532f46b36a8e62027f9da26
Alex Obradovic <alexobrads@gmail.com>
2024-09-11 08:44:51 +1000

    finished

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 Assignment for role at Emmi
 
 
+To execute pipeline
+`spark-submit --packages com.crealytics:spark-excel_2.12:3.3.1_0.18.5 src/pipeline.py ./data SamplePortfolio1 SamplePortfolio2`
+
+
 
--- src/transformations/build_public_markets.py
+++ src/transformations/build_public_markets.py
@@ -6,20 +6,6 @@ def build_public_markets(
         public_universe_df: DataFrame,
         fixed_income_universe_df: DataFrame) -> DataFrame:
 
-    """
-
-    Unions dataframes public_universe and fixed_income_universe into a single view of public markets
-
-    Args:
-        public_universe_df (pyspark.sql.DataFrame): The first integer.
-        fixed_income_universe_df (pyspark.sql.DataFrame): The second integer.
-
-
-    Returns:
-        pyspark.sql.DataFrame: A full view of public markets with schema ../models/public_markets.py
-
-    """
-
     public_universe_df = public_universe_df \
         .withColumn("BondType", lit(None).cast("string")) \
         .select(fixed_income_universe_df.columns)
